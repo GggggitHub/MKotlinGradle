be00201a5c2ebb8cdef3194398a58d6a9c2641a8
zhangyingjie <zhangyingjie@beeboxes.lan>
2022-03-24 09:38:23 +0800

    hello world

---
+++ src/main/kotlin/com/ajie/kotlin/HelloWorld.kt
@@ -0,0 +1,14 @@
+package com.ajie.kotlin
+fun main(){
+    print("Hello world")
+}
+/**
+ * @author Ajie
+ * @date 2022/3/18
+ * @function
+ */
+class HelloWorld {
+    fun main(){
+        print("Hello world")
+    }
+}
